8b463e03d850495a52382a64b19bbe08e8f04b5a
Chip Bell <chip.bell@spinen.com>
2013-10-20 21:12:02 -0400

    Added a JSEND formatting function to the mailer script

--- mailService.php
+++ mailService.php
@@ -1,5 +1,14 @@
 <?php
 
+function formatJsend($status='failure', $message='The response defaults to a failure') {
+	$ary = array(
+		'status' => $status,
+		'message' => $message
+	);
+
+	return json_encode($ary);
+}
+
 // build html email for myself
 $header = "From:contact@aprilandchip.com\r\nContent-type: text/html \r\n";
 $recipient = 'recipient@aprilandchip.com';
@@ -9,7 +18,7 @@ $body = 'HELLO WORLD';
 // attempt to send the email
 if(!mail($recipient, $subject, $body, $header))
 {
-    echo "Mail not sent. Unknown error...";
+	echo formatJsend('error', 'Mail not sent due to an unknown error. Looks like you\'ll have to try again later');
 }
 
 ?>
